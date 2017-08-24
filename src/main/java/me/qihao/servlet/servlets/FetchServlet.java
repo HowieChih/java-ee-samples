576dc8e1fc0028360f92d6ac4a584bd0b8865be3
howiechih <howiechih@qq.com>
2017-08-24 18:05:35 +0800

    # javascript fetch api demo init.

---
+++ src/main/java/me/qihao/servlet/servlets/FetchServlet.java
@@ -0,0 +1,30 @@
+package me.qihao.servlet.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * test javascript fetch api in fetch.html
+ *
+ * @author qihao
+ * @version 1.0 2017-08-24
+ * @since 1.0
+ */
+@WebServlet(name = "fetchServlet", urlPatterns = "/fetch.json")
+public class FetchServlet extends HttpServlet{
+    private static final long serialVersionUID = 390820856557104640L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+        String result = "{\"name\": \"张三\",\"age\": 11}";
+        try (PrintWriter out = resp.getWriter()){
+            out.print(result);
+        }
+    }
+}
