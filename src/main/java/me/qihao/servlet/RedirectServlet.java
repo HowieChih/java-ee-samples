2a34acda46138742e50d52bf1063bed873c0328e
howiechih <howiechih@qq.com>
2017-09-12 14:11:38 +0800

    # add some description of location parameter of servlet response.sendRedirect(String location) function.

--- src/main/java/me/qihao/servlet/RedirectServlet.java
+++ src/main/java/me/qihao/servlet/RedirectServlet.java
@@ -17,7 +17,12 @@ public class RedirectServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("/hello-world");
+        // If the location is relative without a leading '/' the container interprets it as relative to the current request URI.
+        // tips: if current request URI is "/xxx/222", the function value is "hello-world", then it will redirect to "/xxx/hello-world"
+        // If the location is relative with a leading '/' the container interprets it as relative to the servlet container root.
+        // tips: servlet container root from tomcat is the request scheme+"//"+serverName+serverPort http://localhost:8080
+        // If the location is relative with two leading '/' the container interprets it as a network-path reference
+        resp.sendRedirect(req.getContextPath() + "/hello-world");
     }
 
     @Override
